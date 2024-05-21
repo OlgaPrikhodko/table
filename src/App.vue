0a7fa5b3da90f4ffd992ac1648b6f0446e131a02
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-05-21 12:30:11 +0300

    🧹 cleanup unnecessary wrapprer in App.vue and styles in base.css

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@ import TheTable from "@/components/TheTable.vue";
 
 <template>
   <main>
-    <div class="wrapper">
-      <TheTable />
-    </div>
+    <TheTable />
   </main>
 </template>
 
@@ -14,8 +12,5 @@ import TheTable from "@/components/TheTable.vue";
 main {
   width: 100%;
   background-color: #fcfcfc;
-}
-.wrapper {
-  width: 100%;
 }
 </style>
